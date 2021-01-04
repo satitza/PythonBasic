720e664b6b7dd946da16b8abc71d9a929efab424
Satit Porntepanon <st_satitza@hotmail.com>
2021-01-04 22:53:07 +0700

    success for view tutorial dictionary

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+game = {
+    'name': 'GTA V',
+    'price': 300,
+    'hour': 5,
+    'online': True,
+    'character': ['character 1', 'character 2', 'character 3']
+}
+
+if __name__ == "__main__":
+    game['price'] = 315
+    print(len(game))
+    print(game)
+
+    for c in game.get('character'):
+        if c == 'character 2':
+            print(c)
+            break
