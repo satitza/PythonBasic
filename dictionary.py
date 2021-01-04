a8d87e6bd69c05e1505b6025bb25ed62973c773e
Satit Porntepanon <st_satitza@hotmail.com>
2021-01-04 23:05:41 +0700

    success for view tutorial string

--- dictionary.py
+++ dictionary.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     print(game)
 
     for c in game.get('character'):
-        if c == 'character 2':
+        if c in 'character 2':
             print(c)
             break
---
+++ string.py
@@ -0,0 +1,5 @@
+str = 'str1, str2, str3'
+
+if __name__ == "__main__":
+    strs = str.split(', ')
+    print(' + '.join(strs))
