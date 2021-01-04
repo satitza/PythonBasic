362d9c1bd0d39011fa957496519637dd73a2df7c
Satit Porntepanon <st_satitza@hotmail.com>
2021-01-04 22:32:06 +0700

    success for view tutorial list

--- list.py
+++ list.py
@@ -1,2 +1,25 @@
+quest = ['Quest 1', 'Quest 2', 'Quest 3']
+quest_tuple = quest.copy()  # same list but can`t edit element
+
+
+def printQuest(quests):
+    print(len(quests))
+
+    for q in quests:
+        print(q)
+
+    print('*****************************************************')
+
+    i = 1
+    while i <= len(quests):
+        print(quests[i - 1])
+        i += 1
+
+
 if __name__ == "__main__":
-    print('List of python')
+    quest.append('Quest 4')  # add element to last index of list
+    quest.insert(0, 'Quest 0')  # add element to index position of list
+    quest.remove('Quest 4')  # remove element of list
+
+    printQuest(quest)
+    print(quest_tuple)
---
+++ list2.py
@@ -0,0 +1,5 @@
+quest = ['Quest 1', 'Quest 2', 'Quest 3']
+
+if __name__ == "__main__":
+    for q in range(len(quest)):
+        print(str(q + 1) + '. ' + quest[q])
