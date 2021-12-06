7f0bfa3e6cac52b64e884b2a46a880f458587177
Satit Porntepanon <st_satitza@hotmail.com>
2021-12-06 12:39:06 +0700

    Please enter the commit message for your changes. Lines starting

---
+++ DataStructureAndAlgorithm/recursion.py
@@ -0,0 +1,11 @@
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * (factorial(n - 1))
+
+
+if __name__ == "__main__":
+    print("Test recursion function")
+    print(factorial(6))
